96015502e794069754bfe8821086817ee33d70f0
guilhermeR20 <guilherme.rochalima17@gmail.com>
2022-04-06 22:48:55 -0300

    seleção de elementos 1

---
+++ DOM-para-iniciantes/Selecionar-o-dom/script.js
@@ -0,0 +1,10 @@
+// Seleciona pelo ID
+/*const animaisSection = document.getElementById('animais');
+const contatoSection = document.getElementById('contato');
+
+// Retorna null caso não exista
+const naoExiste = document.getElementById('teste');*/
+
+const animais = document.getElementById('animais');
+console.log(animais);
+
